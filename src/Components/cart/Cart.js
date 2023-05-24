50532f7c6c131fea100f76a9d1ad48b4f03bd1d8
MshSiam <siyamkenway@gmail.com>
2023-05-24 20:53:48 +0600

    conditional rendaring

--- src/Components/cart/Cart.js
+++ src/Components/cart/Cart.js
@@ -1,9 +1,16 @@
 import React from "react";
+import "./cart.css";
 
 const Cart = ({ cart, handleRemoveItem }) => {
+  let message;
+  if (cart.length === 0) {
+    message = <p>Please buy somethings</p>;
+  } else {
+    message = <p>Thanks for buying</p>;
+  }
   return (
     <div>
-      <h3>Order Summery</h3>
+      <h3 className={cart.length === 0 ? "" : "orange"}>Order Summery</h3>
       <h5>Order Quantity : {cart.length}</h5>
 
       {cart.map((tshirt) => (
@@ -14,6 +21,9 @@ const Cart = ({ cart, handleRemoveItem }) => {
           </button>
         </p>
       ))}
+      {message}
+      {cart.length === 3 ? <p>well done</p> : ""}
+      {cart.length === 4 && <p>congrats !!</p>}
     </div>
   );
 };
